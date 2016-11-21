bf8e71292eda12f9c6c4bbb540d92192a12a03cf
malkky <arjeti.kraja@gmail.com>
2016-11-21 15:34:25 +0100

    Finalized project

---
+++ app.js
@@ -0,0 +1,59 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    cookieParser = require("cookie-parser"),
+    LocalStrategy = require("passport-local"),
+    flash        = require("connect-flash"),
+    User        = require("./models/user"),
+    session = require("express-session"),
+    methodOverride = require("method-override"),
+    seedDB = require('./seeds')
+    
+//requiring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+    
+mongoose.connect("mongodb://localhost/go_camp");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride('_method'));
+app.use(cookieParser('secret'));
+
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "I have the best dog in the world",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// seedDB();
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.success = req.flash('success');
+   res.locals.error = req.flash('error');
+   next();
+});
+
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+var port = app.get('port');
+app.listen(port, function () {
+    console.log('App running at ' + port);
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+#GoCamp
+
+An express app with user authentication and authorization, new posts, comments and more.
